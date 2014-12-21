84d99a585d20f09d86c53f94e7b0b11d688620e2
Kevin Dungs <kevin@dun.gs>
2014-12-21 23:33:46 +0100

    Fix resizing. Change colors. Add pause screen. Next: Collisions Then: Structure...

--- hexsnake.js
+++ hexsnake.js
@@ -1,15 +1,7 @@
 /*jslint browser: true*/
 (function () {
   'use strict';
-  // Set up the canvas for the game
-  var canvas = document.body.appendChild(document.createElement('canvas'));
-  var resizeCanvas = function () {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-  };
-  resizeCanvas();
-  window.addEventListener('resize', resizeCanvas);
-
+  
   /** A simple 2-dimensional vector */
   var Vec2 = function (x, y) {
     this.x = x;
@@ -76,11 +68,11 @@
   };
 
   HexGrid.prototype.resize = function (w, h) {
-    // w = 2 r
-    // h = √3 r
-    var rw = w / 2 / this.nx,
-        rh = h / Math.sqrt(3) / this.ny;
+    var rw = 2 * w / 3 / this.nx,
+        rh = h / (Math.sqrt(3) * (this.ny + 0.5));
     this.radius = Math.floor(Math.min(rw, rh));
+    this.width = this.nx / 2 * 3 * this.radius;
+    this.height = Math.sqrt(3) * this.radius * (this.ny + 0.5);
   };
 
   HexGrid.prototype.drawHex = function(ctx, coord, fillColor) {
@@ -136,12 +128,16 @@
   // Game
   var game = {
     running: false,
+    gamepad: null,
     colors: {
       snake: '#8BC34A',
       snakeHead: '#4CAF50'
     },
     hexGrid: new HexGrid(30, 20, window.innerWidth, window.innerHeight),
     snake: new Snake(new Vec2(4, 2)),
+    restart: function () {
+      this.snake = new Snake(new Vec2(4, 2));      
+    },
     update: function (ticks) {
       if (!this.running) {
         return;
@@ -150,14 +146,28 @@
         this.snake.move();
       }
     },
-    draw: function (ctx) {
-      if (!this.running) {
-        return;
+    drawPause: function (ctx) {
+      ctx.fillStyle = '#4CAF50';
+      ctx.fillRect(0, 0, this.hexGrid.width, this.hexGrid.height);
+      ctx.fillStyle = '#E3F2FD';
+      ctx.font = '60px Arial';
+      ctx.fillText('HexSnake', 20, 90)
+      ctx.font = '30px Arial';
+      ctx.fillText('Press (A) to resume / pause.', 20, 140);
+      ctx.fillText('Press (B) to restart.', 20, 180);
+      if (this.gamepad === null) {
+        ctx.fillStyle = '#EF9A9A';
+        ctx.fillText('Please connect your Gamepad!.', 20, 260);
       }
+    },
+    drawGame: function (ctx) {
+      ctx.save();
+      ctx.translate(this.hexGrid.radius,
+                    this.hexGrid.radius * Math.sqrt(3) / 2);
       var that = this;
       for (var y = 0; y < 20; y++) {
         for (var x = 0; x < 30; x++) {
-          that.hexGrid.drawHex(ctx, new Vec2(x, y), "#eeeeee");
+          that.hexGrid.drawHex(ctx, new Vec2(x, y), '#C8E6C9');
         }
       }
       var head = that.snake.parts.pop();
@@ -166,16 +176,26 @@
       });
       that.hexGrid.drawHex(ctx, head, that.colors.snakeHead);
       that.snake.parts.push(head);
+      ctx.restore();
+    },
+    draw: function (ctx) {
+      if (!this.running) {
+        this.drawPause(ctx);
+      } else {
+        this.drawGame(ctx);
+      }
     }
   };
 
   // Handle input from gamepad
-  var btnPressed = false;
+  var btnPressed = false,
+      btn2Pressed = false;
   var pollGamepad = function () {
     var gp = navigator.getGamepads()[0],
         axisEW = gp.axes[0],
         axisNS = gp.axes[1],
         btn = gp.buttons[0],
+        btn2 = gp.buttons[1],
         dir = 0;
     if (btn.pressed) {
       btnPressed = true;
@@ -183,10 +203,17 @@
       game.running = !game.running;
       btnPressed = false;
     }
+    if (btn2.pressed) {
+      btn2Pressed = true;
+    } else if (btn2Pressed) {
+      game.restart();
+      game.running = true;
+      btn2Pressed = false;
+    }
     if (axisNS !== 0) {
       var phi = Math.atan2(axisNS, axisEW),
           r = Math.sqrt(axisEW * axisEW + axisNS * axisNS);
-      if (r > 0.1) {
+      if (r > 0.5) {
         if (phi > 0) {
           dir |= HexDirections.MaskS;
         }
@@ -195,27 +222,34 @@
         } else if (Math.abs(phi) >= 3 * Math.PI / 6) {
           dir |= HexDirections.MaskW;
         }
+        game.snake.changeDirection(dir);
       }
-      game.snake.changeDirection(dir);
     }
   };
   var setupGamepad = window.setInterval(function () {
     if (navigator.getGamepads()[0]) {
+      game.gamepad = navigator.getGamepads()[0];
       window.setInterval(pollGamepad, 100);
       window.clearInterval(setupGamepad);
     }
   }, 500);
 
   // Get the context and set up the drawing
+  // Set up the canvas for the game
+  var canvas = document.body.appendChild(document.createElement('canvas'));
+  var resizeCanvas = function () {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    game.hexGrid.resize(window.innerWidth, window.innerHeight);
+  };
+  resizeCanvas();
+  window.addEventListener('resize', resizeCanvas);
   var ctx = canvas.getContext('2d');
   var ticks = 0;
   var drawCtx = function () { 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.save();
-    ctx.translate(game.hexGrid.radius, game.hexGrid.radius * Math.sqrt(3) / 2);
     game.update(ticks++);
     game.draw(ctx);
-    ctx.restore();
     window.setTimeout(function () {
       window.requestAnimationFrame(drawCtx);
     }, 100);
